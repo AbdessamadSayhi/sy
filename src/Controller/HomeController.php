e841aa2431a06baad60b5eb83f351a0218eefa6d
Abdessamad <abdessamadsayhi17@gmail.com>
2024-02-12 11:58:11 +0100

    getting started

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,12 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    //default value ?unknowen
+    #[Route('/home/{name?unknown}', name: 'app_home')]
+    public function index($name): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'name' => $name,
         ]);
     }
 }
