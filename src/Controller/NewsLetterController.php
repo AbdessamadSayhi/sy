a17c1765360c6f520b0337255d5c3655e34e45ef
Abdessamad <abdessamadsayhi17@gmail.com>
2024-02-12 10:07:27 +0100

    basic structure setup

---
+++ src/Controller/NewsLetterController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\TaskType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NewsLetterController extends AbstractController
+{
+    #[Route('subscribe', name: 'newsletter_subscribe')]
+    public function index(): Response
+    {
+        //$form = $this->createForm(NewsLetterType::class);
+
+        return $this->render('news_letter/index.html.twig', [
+            'controller_name' => 'NewsLetterController',
+        ]);
+    }
+}
